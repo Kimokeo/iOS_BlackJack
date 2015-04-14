d5c234885b734f86dcc5418256d834d525cc864e
Kimokeo Rabang <kimokerava@gmail.com>
2015-04-14 12:50:54 -0400

    Blackjack game for iOS in Swift

--- iOS_Black_Jack/Data_Set.swift
+++ iOS_Black_Jack/Data_Set.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+struct DataSet {
+    var deckArray = [
+    "Ace of ♠️":11,
+    "Ace of ♦️":11,
+    "Ace of ♥️":11,
+    "Ace of ♣️":11,
+    "Two of ♠️":2,
+    "Two of ♦️":2,
+    "Two of ♥️":2,
+    "Two of ♣️":2,
+    "Three of ♠️":3,
+    "Three of ♦️":3,
+    "Three of ♥️":3,
+    "Three of ♣️":3,
+    "Four of ♠️":4,
+    "Four of ♦️":4,
+    "Four of ♥️":4,
+    "Four of ♣️":4,
+    "Five of ♠️":5,
+    "Five of ♦️":5,
+    "Five of ♥️":5,
+    "Five of ♣️":5,
+    "Six of ♠️":6,
+    "Six of ♦️":6,
+    "Six of ♥️":6,
+    "Six of ♣️":6,
+    "Seven of ♠️":7,
+    "Seven of ♦️":7,
+    "Seven of ♥️":7,
+    "Seven of ♣️":7,
+    "Eight of ♠️":8,
+    "Eight of ♦️":8,
+    "Eight of ♥️":8,
+    "Eight of ♣️":8,
+    "Nine of ♠️":9,
+    "Nine of ♦️":9,
+    "Nine of ♥️":9,
+    "Nine of ♣️":9,
+    "Ten of ♠️":10,
+    "Ten of ♦️":10,
+    "Ten of ♥️":10,
+    "Ten of ♣️":10,
+    "Jack of ♠️":10,
+    "Jack of ♦️":10,
+    "Jack of ♥️":10,
+    "Jack of ♣️":10,
+    "Queen of ♠️":10,
+    "Queen of ♦️":10,
+    "Queen of ♥️":10,
+    "Queen of ♣️":10,
+    "King of ♠️":10,
+    "King of ♦️":10,
+    "King of ♥️":10,
+    "King of ♣️":10,
+    ]
+    
+    var playerHand = []
+    var dealerHand = []
+    
+}
--- iOS_Black_Jack/ViewController.swift
+++ iOS_Black_Jack/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var playerCards: UILabel!
+    
+    let initialize = DataSet()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        playerCards.text[key:valueForKey(<#key: String#>)] = initialize.deckArray[random(52)]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+        @IBOutlet weak var playerHit: UIButton!
+        playerHand.text += initilize.deckArray[rand(52)]
+    }
 }
 
